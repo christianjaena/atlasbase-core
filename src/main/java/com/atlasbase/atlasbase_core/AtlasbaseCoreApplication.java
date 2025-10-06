994e59811c609b793559f1ed7b7945d7fb3798d0
Christian Jaena <jaenachristian@gmail.com>
2025-10-06 23:14:48 +0800

    Initial project setup

---
+++ src/main/java/com/atlasbase/atlasbase_core/AtlasbaseCoreApplication.java
@@ -0,0 +1,13 @@
+package com.atlasbase.atlasbase_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasbaseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasbaseCoreApplication.class, args);
+	}
+
+}
