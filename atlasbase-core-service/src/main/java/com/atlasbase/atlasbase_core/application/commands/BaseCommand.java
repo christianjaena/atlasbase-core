b72424a67552f72c0af41c7f7940ad31e28eb085
Christian Jaena <jaenachristian@gmail.com>
2025-10-26 16:45:57 +0800

    Bump to Spring Boot v3.5.7

--- atlasbase-core-service/src/main/java/com/atlasbase/atlasbase_core/application/commands/BaseCommand.java
+++ atlasbase-core-service/src/main/java/com/atlasbase/atlasbase_core/application/commands/BaseCommand.java
@@ -1,5 +1,5 @@
 package com.atlasbase.atlasbase_core.application.commands;
 
-public abstract sealed class BaseCommand permits UserSignUpCommand, UserSignInCommand{
+public abstract sealed class BaseCommand permits UserSignUpCommand, UserSignInCommand {
 
 }
