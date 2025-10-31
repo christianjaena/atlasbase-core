28781d445df3d6087cfd6dc16f8f391f7f61c808
Christian Jaena <jaenachristian@gmail.com>
2025-10-31 17:45:52 +0800

    Add JwtProperties

---
+++ atlasbase-core-service/src/main/java/com/atlasbase/atlasbase_core/infrastructure/properties/JwtProperties.java
@@ -0,0 +1,12 @@
+package com.atlasbase.atlasbase_core.infrastructure.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.jwt")
+public record JwtProperties(String secret,
+                            String issuer,
+                            String tokenPrefix,
+                            long expiration,
+                            long refreshExpiration
+) {
+}
