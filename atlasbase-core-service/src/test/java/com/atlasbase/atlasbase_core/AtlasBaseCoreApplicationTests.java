24da4eda91a34e36da9df6684f9213940d53ed67
Christian Jaena <jaenachristian@gmail.com>
2025-12-29 13:52:03 +0800

    Add @ShouldNotPin test

--- atlasbase-core-service/src/test/java/com/atlasbase/atlasbase_core/AtlasBaseCoreApplicationTests.java
+++ atlasbase-core-service/src/test/java/com/atlasbase/atlasbase_core/AtlasBaseCoreApplicationTests.java
@@ -1,13 +1,23 @@
 package com.atlasbase.atlasbase_core;
 
+import me.escoffier.loom.loomunit.LoomUnitExtension;
+import me.escoffier.loom.loomunit.ShouldNotPin;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.devtools.restart.enabled=false")
+@ExtendWith(LoomUnitExtension.class)
 class AtlasBaseCoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@ShouldNotPin
+	void shouldNotPin() throws Exception {
+		AtlasBaseCoreApplication.main(new String[] {});
 	}
 
 }
